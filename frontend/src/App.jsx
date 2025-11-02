5f51502b9a73f38e4131380a33eb1fe6daf08bd8
Rakibul Islam <rakibulislammondal21@gmail.com>
2025-11-02 20:32:41 +0530

    Added Tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      WELCOME TO COOCHBEHAR GOVERNMENT ENGINEERING COLLEGE
+      <h1 className="text-amber-700 bg-amber-300">
+        WELCOME TO COOCHBEHAR GOVERNMENT ENGINEERING COLLEGE
+      </h1>
     </div>
   );
 }
